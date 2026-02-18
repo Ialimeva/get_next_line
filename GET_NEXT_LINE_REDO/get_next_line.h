6edc8787a130639e5ce2b603708e25fcaab3ab38
Ialimeva Randrianasolo <dattetata90@gmail.com>
2026-02-18 15:48:00 +0300

    Add files via upload

---
+++ GET_NEXT_LINE_REDO/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialrandr <ialrandr@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 10:16:11 by ialrandr          #+#    #+#             */
+/*   Updated: 2026/02/18 15:30:28 by ialrandr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*buffer_read(int buffer, int fd);
+int		find_newline(char *buffer_line);
+char	*extract_line(char *buffer_line);
+size_t	ft_strlen(char *s);
+char	*leftover(char *buffer_line, char *extracted_line);
+char	*string_combo(char *str1, char *str2);
+char	*read_until_newline(char *buffer_line, int fd);
+char	*empty_leftover(char **buffer_line, char **leftover_line, int fd);
+
+#endif
